13a08f8ec5f9ba96901db138241775d1f72848cf
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-03-08 12:39:05 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+alert("hi")
+
+
+const a = 5;
+const b = 2;
+const Myname = "nico";
+// const = constant 상수 
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello"+"june")
+
+console.log("your"+Myname)
